1fe43f017df6967d6698af5e64de7c4389b156dd
Jack <djcube7@gmail.com>
2011-08-11 14:55:37 +0100

    added numbers around the grid

--- grid.rb
+++ grid.rb
@@ -1,7 +1,8 @@
 class Grid
-  attr_reader :grid
+  attr_reader :grid, :grid_size
   
   def initialize(grid_size)
+    @grid_size = grid_size
     @grid = []
     (1..grid_size).each do |i|
       row = []
@@ -14,7 +15,11 @@ class Grid
   
   def print_grid
     puts
-    grid.each do |row|
+    print "  "
+    [*(1..grid_size)].each { |i| print "#{i} "}
+    puts
+    grid.each_with_index do |row,i|
+      print "#{i+1} "
       row.each do |cell|
         print cell
       end
